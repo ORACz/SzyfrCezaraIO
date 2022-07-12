dd33438f662d16df9857cac32cc6593f6abba3ea
ORACz <oracz.piotrek@gmail.com>
2022-07-12 23:46:02 +0200

    Initial commit

---
+++ src/IO/SzyfrCezaraIO.java
@@ -0,0 +1,64 @@
+package IO;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class SzyfrCezaraIO {
+    String text;
+    String textPoZmianie;
+
+    public SzyfrCezaraIO() throws FileNotFoundException {
+
+        File file = new File("C://Users//Piotr//Desktop//Dokument.txt");
+        String text = "";
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()){
+            text+=scanner.nextLine();
+        }
+        this.text = text;
+        scanner.close();
+
+        szyfrujemy();
+
+        File output = new File("C://Users//Piotr//Desktop//DokumentOut.txt");
+        PrintWriter printWriter = new PrintWriter(output);
+        printWriter.println(this.textPoZmianie);
+        printWriter.close();
+
+
+
+    }
+    public void szyfrujemy(){
+        String text = "";
+        char [] tab = this.text.toCharArray();
+        int ascii;
+        for (int i =0; i<tab.length; i++){
+            ascii = (int) tab[i];
+
+            if (ascii>= 65 && ascii<= 87){
+                ascii+=3;
+                text+= (char) ascii;
+
+            } else if (ascii>=88 && ascii<=90) {
+                ascii-=23;
+                text += (char) ascii;
+            }else if (ascii>=97 && ascii<= 119){
+                ascii+=3;
+                text+= (char) ascii;
+            } else if (ascii>=120 && ascii<=122) {
+                ascii-=23;
+                text += (char) ascii;
+            }
+        }
+        this.textPoZmianie = text;
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        SzyfrCezaraIO kod = new SzyfrCezaraIO();
+    }
+}
+
